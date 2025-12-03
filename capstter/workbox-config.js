7fba3bc3022d371f3836871baecb252acf6200a8
eggie18 <chromwellicetipura@gmail.com>
2025-12-03 20:17:47 +0800

    PWA might bug late

---
+++ capstter/workbox-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  globDirectory: 'dist/',
+  globPatterns: [
+    '**/*.{js,css,html,svg,png,ico,json}'
+  ],
+  swSrc: 'src/sw.js',
+  swDest: 'dist/sw.js',
+  maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+  mode: 'production'
+};
